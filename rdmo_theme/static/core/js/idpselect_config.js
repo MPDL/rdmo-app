f7d91b7747cd0b5e3e587901e4c153864529f190
vilarodr <vilarodriguez@mpdl.mpg.de>
2021-11-19 12:12:48 +0100

    Login SSO Form with idpselect embedded

--- rdmo_theme/static/core/js/idpselect_config.js
+++ rdmo_theme/static/core/js/idpselect_config.js
@@ -9,7 +9,7 @@ function IdPSelectUIParms(){
     this.defaultLanguage = 'de';     // Language to use if the browser local doesnt have a bundle
     this.defaultLogo = 'https://rdmo.mpdl.mpg.de/static/core/img/header/mpg-logo-vector.svg';  // Replace with your own logo
     this.defaultLogoWidth = 196;
-    this.defaultLogoHeight = 54 ;
+    this.defaultLogoHeight = 196 ;
     this.defaultReturn = null;       // If non null, then the default place to send users who are not
                                      // Approaching via the Discovery Protocol for example
     //this.defaultReturn = "https://example.org/Shibboleth.sso/DS?SAMLDS=1&target=https://example.org/secure";
